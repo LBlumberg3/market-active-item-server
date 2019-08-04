36a4bdbb04ff4b40b7273f3751ec83c38dc4ab79
LBlumberg3 <tlblumberg@gmail.com>
2019-08-04 09:33:30 -0500

    initial boostrapping of project

---
+++ src/main/java/com/leo/market/items/activeitemserver/ActiveItemServerApplication.java
@@ -0,0 +1,13 @@
+package com.leo.market.items.activeitemserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActiveItemServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActiveItemServerApplication.class, args);
+	}
+
+}
