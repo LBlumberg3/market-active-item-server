7debf49d1c4c4d3a8d37b3fdc3b03f63feed12e5
LBlumberg3 <tlblumberg@gmail.com>
2019-08-04 10:15:58 -0500

    added the tomcat started (that I forgot), updated main to start up the server, changed packaging to jar (for later ease of containerization

--- src/main/java/com/leo/market/items/activeitemserver/ActiveItemServerApplication.java
+++ src/main/java/com/leo/market/items/activeitemserver/ActiveItemServerApplication.java
@@ -2,12 +2,19 @@ package com.leo.market.items.activeitemserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ActiveItemServerApplication {
+public class ActiveItemServerApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ActiveItemServerApplication.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(ActiveItemServerApplication.class, args);
+    }
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(application.getClass());
 	}
 
 }
